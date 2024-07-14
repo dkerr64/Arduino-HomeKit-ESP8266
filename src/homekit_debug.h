35cc8a60f65175f31dd530ca0d7b3d9385abf9c6
David Kerr <david@kerr.net>
2024-07-14 17:08:43 -0400

    log message timestamps should be unsigned long

--- src/homekit_debug.h
+++ src/homekit_debug.h
@@ -36,7 +36,7 @@ void logToBuffer_P(const char *fmt, ...);
 #define DEBUG(message, ...)  HOMEKIT_PRINTF(">>> %s: " message "\n", __func__, ##__VA_ARGS__)
 static uint32_t start_time = 0;
 #define DEBUG_TIME_BEGIN()  start_time=millis();
-#define DEBUG_TIME_END(func_name)  HOMEKIT_PRINTF("### [%7d] %s took %6dms\n", millis(), func_name, (millis() - start_time));
+#define DEBUG_TIME_END(func_name)  HOMEKIT_PRINTF("### [%7lu] %s took %6lu ms\n", millis(), func_name, (millis() - start_time));
 #define DEBUG_HEAP() DEBUG("Free heap: %d", system_get_free_heap_size());
 
 #else
@@ -50,7 +50,7 @@ static uint32_t start_time = 0;
 
 #if HOMEKIT_LOG_LEVEL >= HOMEKIT_LOG_ERROR
 
-#define ERROR(message, ...) HOMEKIT_PRINTF("!!! [%7d] HomeKit: " message "\n", millis(), ##__VA_ARGS__)
+#define ERROR(message, ...) HOMEKIT_PRINTF("!!! [%7lu] HomeKit: " message "\n", millis(), ##__VA_ARGS__)
 
 #else
 
@@ -60,7 +60,7 @@ static uint32_t start_time = 0;
 
 #if HOMEKIT_LOG_LEVEL >= HOMEKIT_LOG_INFO
 
-#define INFO(message, ...) HOMEKIT_PRINTF(">>> [%7d] HomeKit: " message "\n", millis(), ##__VA_ARGS__)
+#define INFO(message, ...) HOMEKIT_PRINTF(">>> [%7lu] HomeKit: " message "\n", millis(), ##__VA_ARGS__)
 #define INFO_HEAP() INFO("Free heap: %d", system_get_free_heap_size());
 
 #else
